02fa1e938808cc700572089e51778bccbcb72eb7
Kos1111Kos <konstantin.smirnov.2011@gmail.com>
2024-03-12 17:53:23 +0200

    add fix1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,28 +3,29 @@ import { BiSolidPhone } from "react-icons/bi";
 import styles from "./Contact.module.css";
 
 export default function Contacts({ contacts, deleteContact }) {
-  return contacts.map(({ id, name, number }) => {
-    return (
-      <li
-        key={id}
-        className={styles.userItem}
-        onClick={(evt) => deleteContact(id, evt)}
-      >
-        <div>
-          <div className={styles.nameContainer}>
-            <FaUserNinja />
-            <p>{name}</p>
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li
+          key={id}
+          className={styles.userItem}
+          onClick={(evt) => deleteContact(id, evt)}
+        >
+          <div>
+            <div className={styles.nameContainer}>
+              <FaUserNinja />
+              <p>{name}</p>
+            </div>
+            <div className={styles.phoneContainer}>
+              <BiSolidPhone />
+              <p>{number}</p>
+            </div>
           </div>
-          <div className={styles.phoneContainer}>
-            <BiSolidPhone />
-            <p>{number}</p>
-          </div>
-        </div>
-
-        <button type="button" className={styles.deleteButton}>
-          Delete
-        </button>
-      </li>
-    );
-  });
+          <button type="button" className={styles.deleteButton}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
